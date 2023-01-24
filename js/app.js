ef053592c6de1f7a732824be097b9e711dc11c42
luisbgonzalez21 <luisbg809@gmail.com>
2023-01-24 16:51:30 +0000

    updated navigation
    in this update there is a new, responsive navigation in the header, that collapses for mobile view.

--- js/app.js
+++ js/app.js
@@ -11,3 +11,17 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  const primaryNav = document.querySelector(".primary-navigation");
+  const navToggle = document.querySelector(".mobile-nav-toggle");
+  navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+      primaryNav.setAttribute("data-visible", true);
+      navToggle.setAttribute("aria-expanded", true);
+    } else if (visibility === "true") {
+      primaryNav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+    }
+  });
